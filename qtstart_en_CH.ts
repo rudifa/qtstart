7b880addb2393fe63ed4e061212fdced2d7ada4f
Rudi Farkas <rudi.farkas@gmail.com>
2024-12-22 21:31:22 +0100

    add Svg t the project; rename it to qtstart

---
+++ qtstart_en_CH.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_CH">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/Qt_6_8_1_for_macOS-Debug/untitled_autogen/include/ui_mainwindow.h" line="51"/>
+        <location filename="build/qtstart_autogen/include/ui_mainwindow.h" line="51"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
